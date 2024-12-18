ceb99e2cd600d2b30b02c8110f7364e39d551f07
daniil <daniil.me@gmail.com>
2024-12-18 22:52:23 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import telebot
+from config import botkey
+bot = telebot.TeleBot(botkey)
+
+@bot.message.handler(commands=['start'])
+def handle_message(message):
+  bot.send_message(message.chat.id, 'Hi!')
+
+@bot.message_handler(command=['help'])
+def handler_message(message):
+  bot.send_message(message.chat.id, 'Test - BOT!')
+bot.polling()
+  
